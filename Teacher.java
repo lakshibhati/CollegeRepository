d62dfeca681c19c53484efacf190a17ad6af0755
Lakshi Bhati <lakshi.bhati@ibm.com>
2022-06-06 11:49:58 +0530

    name to last name and first name

--- Teacher.java
+++ Teacher.java
@@ -9,8 +9,13 @@ public class Teacher
 
 		System.out.println("Enter Id");
 		int id=sc.nextInt();
-		System.out.print("Enter name:");
-		String name=sc.nextLine();
+		int id=sc.nextInt();
+		System.out.print("Enter first name:");
+		String fname=sc.nextLine();
+
+		System.out.print("Enter last name:");
+		String lname=sc.nextLine();
+
 		System.out.print("Enter Subject:");
 		String subject=sc.nextLine();
 		
