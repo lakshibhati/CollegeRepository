c8b6fb5a1a425b183e1f527bf973a24856f8b0a1
Lakshi Bhati <lakshi.bhati@ibm.com>
2022-06-06 11:43:08 +0530

    name to first name and last name

---
+++ Student.java
@@ -0,0 +1,22 @@
+public class Student
+{
+	public static void main(String args[])
+	{
+		
+		
+		Scanner sc=new Scanner(System.in);
+		
+
+		System.out.println("Enter Id");
+		int id=sc.nextInt();
+		System.out.print("Enter first name:");
+		String fname=sc.nextLine();
+
+		System.out.print("Enter last name:");
+		String lname=sc.nextLine();
+
+		System.out.print("Enter class:");
+		int class=sc.nextInt();
+		
+	}
+}
